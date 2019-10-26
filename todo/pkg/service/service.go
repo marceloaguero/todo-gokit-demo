ff052bf853019dc107bf806bee020381f003f0a7
Marcelo Aguero <marceloaguero@gmail.com>
2019-10-26 19:41:32 -0300

    Add service interface

--- todo/pkg/service/service.go
+++ todo/pkg/service/service.go
@@ -1,7 +1,16 @@
 package service
 
+import (
+	"context"
+
+	"github.com/marceloaguero/todo-gokit-demo/todo/pkg/io"
+)
+
 // TodoService describes the service.
 type TodoService interface {
-	// Add your methods here
-	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+	Get(ctx context.Context) (t []io.Todo, error error)
+	Add(ctx context.Context, todo io.Todo) (t io.Todo, error error)
+	SetComplete(ctx context.Context, id string) (error error)
+	RemoveComplete(ctx context.Context, id string) (error error)
+	Delete(ctx context.Context, id string) (error error)
 }
