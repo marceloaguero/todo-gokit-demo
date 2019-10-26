3c04fe8786f7fce3f933ec579f140c77e50fa0b8
Marcelo Aguero <marceloaguero@gmail.com>
2019-10-26 19:52:19 -0300

    Add generation

---
+++ todo/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "/home/marcelo/go/src/github.com/marceloaguero/todo-gokit-demo/todo/cmd/service"
+
+func main() {
+	service.Run()
+}
--- todo/pkg/service/service.go
+++ todo/pkg/service/service.go
@@ -14,3 +14,40 @@ type TodoService interface {
 	RemoveComplete(ctx context.Context, id string) (error error)
 	Delete(ctx context.Context, id string) (error error)
 }
+
+type basicTodoService struct{}
+
+func (b *basicTodoService) Get(ctx context.Context) (t []io.Todo, error error) {
+	// TODO implement the business logic of Get
+	return t, error
+}
+func (b *basicTodoService) Add(ctx context.Context, todo io.Todo) (t io.Todo, error error) {
+	// TODO implement the business logic of Add
+	return t, error
+}
+func (b *basicTodoService) SetComplete(ctx context.Context, id string) (error error) {
+	// TODO implement the business logic of SetComplete
+	return error
+}
+func (b *basicTodoService) RemoveComplete(ctx context.Context, id string) (error error) {
+	// TODO implement the business logic of RemoveComplete
+	return error
+}
+func (b *basicTodoService) Delete(ctx context.Context, id string) (error error) {
+	// TODO implement the business logic of Delete
+	return error
+}
+
+// NewBasicTodoService returns a naive, stateless implementation of TodoService.
+func NewBasicTodoService() TodoService {
+	return &basicTodoService{}
+}
+
+// New returns a TodoService with all of the expected middleware wired in.
+func New(middleware []Middleware) TodoService {
+	var svc TodoService = NewBasicTodoService()
+	for _, m := range middleware {
+		svc = m(svc)
+	}
+	return svc
+}
