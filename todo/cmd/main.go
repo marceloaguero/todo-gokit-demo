a036942f47daacc96fc46adb6135b84798eaa91c
Marcelo Aguero <marceloaguero@gmail.com>
2019-10-26 19:54:49 -0300

    Fix paths

--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-import service "/home/marcelo/go/src/github.com/marceloaguero/todo-gokit-demo/todo/cmd/service"
+import service "github.com/marceloaguero/todo-gokit-demo/todo/cmd/service"
 
 func main() {
 	service.Run()
